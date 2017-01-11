dcfbbf668458702d6566ae688d1a9ac2276a2644
will <william.tan@honeywell.com>
2017-01-11 13:45:23 +0800

    move Octopuslib to another repo

--- deploy.ps1
+++
@@ -1,6 +0,0 @@
-$global:libraryname="Service Configuration RabbitMQ"
-$replacevalue="v1.0.0-rc55"
-$variablename="serviceversion_rabbitmq"
-set-location .\lib
-. .\Octopuslib.ps1
-update-octopusvariable $libraryname $variablename $replacevalue
